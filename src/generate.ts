f2894f2df24a7aedaf2948a07e417c6d5341f6c8
Aelpxy <84912564+Aelpxy@users.noreply.github.com>
2022-11-04 13:04:50 +0000

    format code

--- src/generate.ts
+++ src/generate.ts
@@ -4,22 +4,21 @@ const colorHash = new ColorHash({ saturation: 1.0 });
 const stringToColor = (str: string): string => colorHash.hex(str);
 
 export const generateColor = (str: string): [string, string] => {
-    const st1 = str.substring(0, str.length / 2);
-    const st2 = str.substring(str.length / 2);
-    const colorOne = stringToColor(st1);
-    const colorTwo = stringToColor(st2);
-  
-    return [colorOne, colorTwo];
+  const st1 = str.substring(0, str.length / 2);
+  const st2 = str.substring(str.length / 2);
+  const colorOne = stringToColor(st1);
+  const colorTwo = stringToColor(st2);
+
+  return [colorOne, colorTwo];
 };
 
 const generateImage = (str: string) => {
-    const size = 512
-    const [colorOne, colorTwo] = generateColor(str);
-    const Image = `
+  const size = 512
+  const [colorOne, colorTwo] = generateColor(str);
+  const Image = `
     <svg width="${size}" height="${size}" viewBox="0 0 ${size} ${size}" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <circle cx="${size / 2}" cy="${size / 2}" r="${
-        size / 2
-      }" fill="url(#gradient)" />
+      <circle cx="${size / 2}" cy="${size / 2}" r="${size / 2
+    }" fill="url(#gradient)" />
       <defs>
         <linearGradient id="gradient" x1="0" y1="0" x2="${size}" y2="${size}" gradientUnits="userSpaceOnUse">
           <stop stop-color="${colorOne}" />
@@ -29,7 +28,7 @@ const generateImage = (str: string) => {
     </svg>
       `.trim();
 
-      return Image;
+  return Image;
 }
 
 export default generateImage;
